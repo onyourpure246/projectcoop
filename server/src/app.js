d541e0de829460dea853668e6cd684af79061403
onyourpure246 <natanon.8261@gmail.com>
2024-12-27 13:19:09 +0700

    first

---
+++ server/src/app.js
@@ -0,0 +1,18 @@
+console.log("Hello World")
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+// const dbConfig = require('./config/db.config')
+const app = express()
+app.use(morgan('combine'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.post('/register', (req, res) => {
+    res.send({ message: 'Hello! ' + req.body.studentid + ', you are now registered!' })
+})
+
+app.listen(process.env.PORT || 8081)
